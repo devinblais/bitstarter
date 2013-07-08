49f531e0a346b5d525940edbe53b2ac70e3e0946
Devin Blais <devinblais+git@gmail.com>
2013-07-07 20:03:19 -0700

    Adding ability to check urls and files

--- grader.js
+++ grader.js
@@ -24,8 +24,17 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertURLExists = function(inURL) {
+    if(inURL.length === 0) {
+        console.log("URL is blank. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return inURL;
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,8 +53,7 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkForParts = function ($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -53,7 +61,27 @@ var checkHtmlFile = function(htmlfile, checksfile) {
         out[checks[ii]] = present;
     }
     return out;
+
+}
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    return checkForParts($,checksfile)
 };
+
+var checkURL = function(url,checksfile) {
+  rest.get(url).on('complete', function(result) {
+  if (result instanceof Error) {
+    console.log("Could not load url: %s", results.message);
+    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+  } else {
+    $ = cheerio.load(result)
+    var checkJson = checkForParts($, checksfile)
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  }
+});
+}
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -65,10 +93,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL of a webpage to load', clone(assertURLExists))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+      checkURL(program.url, program.checks);
+    } else {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
